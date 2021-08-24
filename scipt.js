68bd5d40ff3a4470e077a108420ac12ab5ef263c
Michaela <michaelakodes@gmail.com>
2021-08-24 13:29:55 -0400

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("test")[0];
+document.getElementById("demo").innerHTML;
+document.createElement("LI");
+newItem.appendChild(textnode);
+document.getElementById("myH1").style.color;
