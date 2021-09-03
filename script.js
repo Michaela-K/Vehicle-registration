e31d135cbcc342e3597d432af3c979309d6c6304
Michaela-K <michaelakodes@gmail.com>
2021-09-03 13:16:29 -0400

    added- name reset on submit, W.I.P- createElement

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let sizeVehicleEL = document.getElementsByClassName("sizeVehicle")[0];
 // let vehicleSizeEl = document.getElementsByClassName("vehicleSize")[0];
 
 const submitEl = document.getElementById("submit");
-const incrementEl = document.getElementById("add");
-const decrementEl = document.getElementById("minus");
+const incrementEl = document.getElementById("add-card");
+const decrementEl = document.getElementById("minus-card");
 
 //Default Status
 greetingEl.classList.add("hidden");
@@ -25,15 +25,13 @@ submitEl.addEventListener("click", function () {
     console.log(`${firstNameEl}`);
     greetingEl.classList.remove("hidden");
     greetingEl.innerText = `Salams  ${firstNameEl} ${lastNameEl}! Thank's for joining us!`;
-    // document.createElement("p")= `Salams  ${firstNameEl} ${lastNameEl}! Thank's for joining us!`;
-    firstNameEl.innerText = ""; //how do i get this to work
-    lastNameEl = 0;
+    firstNameEl = document.getElementById("firstName").value = "";
+    lastNameEl = document.getElementById("lastName").value = "";
   } else {
     alert("Please enter a name that is at least 3 characters long");
   }
 });
 firstLastNamesEl.addEventListener("keydown", function (e) {
-  //   let firstLastNamesEl = document.getElementsByClassName("firstlastNames")[0];
   let firstNameEl = document.getElementById("firstName").value;
   let lastNameEl = document.getElementById("lastName").value;
   if (e.key === "Enter") {
@@ -63,30 +61,30 @@ decrementEl.addEventListener("click", function () {
 
 //when enter the length and width of the car, box size should change
 
-sizeVehicleEL.addEventListener("keydown", function (f) {
-  if (f.key === "Enter") {
-    console.log("Enter key pressed");
-    let lengthEL = document.getElementById("length").value;
-    let widthEL = document.getElementById("width").value;
-    if (lengthEL <= 100 && widthEL <= 60) {
-      alert(`Please enter valid vehicle dimensions`);
-    } else if (lengthEL <= 140 && widthEL <= 63) {
-      sizeVehicleEL.innerText = `Economy Vehicle`;
-      document.getElementsByClassName("vehicleSize")[0].style.width = "100px";
-    } else if (lengthEL <= 160 && widthEL <= 65) {
-      sizeVehicleEL.innerText = `Compact Vehicle`;
-      document.getElementsByClassName("vehicleSize")[0].style.width = "200px";
-    } else if (lengthEL <= 180 && widthEL <= 71) {
-      sizeVehicleEL.innerText = `Mid Size Vehicle`;
-      document.getElementsByClassName("vehicleSize")[0].style.width = "300px";
-    } else if (lengthEL <= 192 && widthEL <= 75) {
-      sizeVehicleEL.innerText = `Full Size Vehicle`;
-      document.getElementsByClassName("vehicleSize")[0].style.width = "400px";
-    } else if (lengthEL > 192 && widthEL > 75) {
-      sizeVehicleEL.innerText = `Vehicle not on File`;
-    }
-  }
-});
+// sizeVehicleEL.addEventListener("keydown", function (f) {
+//   if (f.key === "Enter") {
+//     console.log("Enter key pressed");
+//     let lengthEL = document.getElementById("length").value;
+//     let widthEL = document.getElementById("width").value;
+//     if (lengthEL <= 100 && widthEL <= 60) {
+//       alert(`Please enter valid vehicle dimensions`);
+//     } else if (lengthEL <= 140 && widthEL <= 63) {
+//       sizeVehicleEL.innerText = `Economy Vehicle`;
+//       document.getElementsByClassName("vehicleSize")[0].style.width = "100px";
+//     } else if (lengthEL <= 160 && widthEL <= 65) {
+//       sizeVehicleEL.innerText = `Compact Vehicle`;
+//       document.getElementsByClassName("vehicleSize")[0].style.width = "200px";
+//     } else if (lengthEL <= 180 && widthEL <= 71) {
+//       sizeVehicleEL.innerText = `Mid Size Vehicle`;
+//       document.getElementsByClassName("vehicleSize")[0].style.width = "300px";
+//     } else if (lengthEL <= 192 && widthEL <= 75) {
+//       sizeVehicleEL.innerText = `Full Size Vehicle`;
+//       document.getElementsByClassName("vehicleSize")[0].style.width = "400px";
+//     } else if (lengthEL > 192 && widthEL > 75) {
+//       sizeVehicleEL.innerText = `Vehicle not on File`;
+//     }
+//   }
+// });
 
 //when you click a colour the colour should appear in the colour box
 //First set
@@ -132,3 +130,94 @@ function redColor2() {
 
 // document.createElement("LI");   not working above??
 // newItem.appendChild(textnode);???
+
+//ADD OR MINUS CARDS
+const addCard = document.getElementById("add-card");
+addCard.addEventListener("click", function () {
+  const addValue = document.getElementsByClassName("number")[0].innerHTML;
+  console.log("addValue", addValue);
+  addCards();
+});
+
+function addCards() {
+  //create elements
+  const vehicle = document.createElement("div");
+  const div1 = document.createElement("div");
+  //what colour would you prefer?
+  let h2a = document.createElement("h2");
+  //color buttons
+  const colorBtns = document.createElement("div");
+  const div2 = document.createElement("div");
+  const greenBtn = document.createElement("button");
+  const greyBtn = document.createElement("button");
+  const yellowBtn = document.createElement("button");
+  const redBtn = document.createElement("button");
+  //Size of Vehicle
+  const div3 = document.createElement("div");
+  let h2b = document.createElement("h2");
+  //Length Width Inputs
+  const sizeVehicle = document.createElement("div");
+  const div4 = document.createElement("div");
+  const inputLength = document.createElement("input");
+  inputLength.placeholder = "Length(in)";
+  const div5 = document.createElement("div");
+  const inputWidth = document.createElement("input");
+  inputWidth.placeholder = "Width(in)";
+  //Car Size & color
+  const vehicleSize = document.createElement("div");
+  const p = document.createElement("p");
+
+  const addValue = document.getElementsByClassName("number")[0].innerHTML;
+  //add content
+  h2a.textContent = `What color would you prefer for vehicle ${addValue}?`;
+  greenBtn.textContent = "Green";
+  greyBtn.textContent = "Grey";
+  yellowBtn.textContent = "Yellow";
+  redBtn.textContent = "Red";
+  h2b.textContent = "What is the size of the car?";
+  p.textContent = "Car Size & Color";
+
+  //append to DOM
+  vehicle.appendChild(div1);
+  div1.appendChild(h2a);
+  vehicle.appendChild(colorBtns);
+  colorBtns.appendChild(div2);
+  div2.appendChild(greenBtn);
+  div2.appendChild(greyBtn);
+  div2.appendChild(yellowBtn);
+  div2.appendChild(redBtn);
+  vehicle.appendChild(div3);
+  div3.appendChild(h2b);
+  vehicle.appendChild(sizeVehicle);
+  sizeVehicle.appendChild(div4);
+  div4.appendChild(inputLength);
+  sizeVehicle.appendChild(div5);
+  div5.appendChild(inputWidth);
+  vehicle.appendChild(vehicleSize);
+  vehicleSize.appendChild(p);
+
+  const vehicles = document.querySelector(".vehicles");
+  vehicles.appendChild(vehicle);
+}
+const minusCard = document.getElementById("minus-card");
+minusCard.addEventListener("click", function () {
+  const minusValue = document.getElementsByClassName("number")[0].innerHTML;
+  console.log("minusValue", minusValue);
+});
+
+// //create a new div and set its attributes
+// let div = document.createElement("div");
+// div.id = "content";
+// div.className = "note";
+
+// // create a new text node and add it to the div
+// let text = document.createTextNode("CreateElement example");
+// div.appendChild(text);
+
+// // create a new heading and add it to the div
+// let h2 = document.createElement("h2");
+// h2.textContent = "Add h2 element to the div";
+// div.appendChild(h2);
+
+// // add div to the document
+// document.body.appendChild(div);
